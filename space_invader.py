0ea06b2b1a71a2e28b4974efefbbfbe025890b3d
Baptiste RENOUARD <baptisterenouard7@gmail.com>
2021-12-13 15:23:32 +0100

    objet2

--- space_invader.py
+++ space_invader.py
@@ -1,13 +1,16 @@
-from tkinter import *
+import tkinter as Tk
 
-fen_princ = Tk()
+fen_princ = Tk.Tk()
 
 fen_princ.title("SPACE INVADER")
 
-fen_princ.geometry("600x600")
+fen_princ.geometry("1000x1000")
 
-monCanvas = Canvas(fen_princ, width=500, height=500, bg='ivory')
+monCanvas = Tk.Canvas(fen_princ, width=600, height=600, bg='ivory')
 
 monCanvas.pack()
 
 fen_princ.mainloop()
+score=100
+scoreLabel = Tk.Label(fen_princ, textvariable = score)
+scoreLabel.pack()
