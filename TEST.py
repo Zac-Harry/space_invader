15557054fe3f718c2e48ba27def15debd6b3f2f3
Baptiste RENOUARD <baptisterenouard7@gmail.com>
2021-12-13 16:14:47 +0100

    edolf

---
+++ TEST.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+
+
+class fen():
+    def __init__(self):
+        self.main_haut = "1600"                       
+        self.main_larg = "900"
+        self.canvas = ""
+        self.canvas_haut = "1280"                      
+        self.canvas_larg = "700" 
+        self.score = 0    
+        self.score_label = ""
+        self
+        #self.AfficherFenetre()
+
+    def AfficherFenetre(self):
+        self.main=tk.Tk()
+        self.main.geometry(self.main_haut+"x"+self.main_larg)
+        self.canvas = tk.Canvas(self.main, width = self.canvas_haut, height = self.canvas_larg, bg ='black')
+        self.canvas.pack()   
+        self.score_label=tk.Label(self.main , text="Score : "+ str(self.score) )
+        self.score_label.pack(side=tk.RIGHT) 
+        boutonQuit = tk.Button(self.main , text='Quitter',command=self.quitter)
+        boutonQuit.pack()
+        self.background = tk.PhotoImage(file="fond.png")
+        self.canvas.create_image(0, 0, image=self.background, anchor='nw')
+        
+        tk.mainloop()
+    def quitter(self):
+        
+        self.canvas.delete()     
+        self.main.destroy()         
+        exit()       
+
+MW = fen()
+MW.AfficherFenetre()
