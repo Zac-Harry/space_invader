8fd65b6b79d972d182a0a8a18f30194c3115849d
GaspardBSG <gaspardbsg@gmail.com>
2022-01-10 16:08:58 +0100

    committerand

--- block.py
+++ block.py
@@ -1,16 +1,3 @@
-from os import read
-
-
 class block():
-    def __init__(self,posX1,posY1,posX2,posY2,invincible,color):
-        self.x1 = posX1
-        self.y1 = posY1
-        self.x2 = posX2
-        self.y2 = posY2
-
-        self.__color_fill = "red"
-        self.vie=30
-
-  
-
-
+    def __init__(self) -> None:
+        pass
